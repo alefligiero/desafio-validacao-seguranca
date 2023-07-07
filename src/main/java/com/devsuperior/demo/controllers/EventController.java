a5fbf06c31bb0482d03c8ce67ad9ee3efac79fe6
alefligiero <alefligieroagb@gmail.com>
2023-07-07 15:17:37 -0300

    Event & City Reposotories, Controllers, Service

---
+++ src/main/java/com/devsuperior/demo/controllers/EventController.java
@@ -0,0 +1,34 @@
+package com.devsuperior.demo.controllers;
+
+import com.devsuperior.demo.dto.EventDTO;
+import com.devsuperior.demo.services.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/events")
+public class EventController {
+
+    @Autowired
+    private EventService eventService;
+
+    @GetMapping
+    public ResponseEntity<Page<EventDTO>> findAll(Pageable pageable) {
+        Page<EventDTO> list = eventService.findAllPaged(pageable);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<EventDTO> insert(@RequestBody EventDTO dto) {
+        dto = eventService.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+}
